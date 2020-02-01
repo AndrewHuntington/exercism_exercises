110a03fdfd79c7874811965f639d557ebc3c42ba
Andrew Huntington <andrew.huntington@gmail.com>
2020-02-01 22:36:30 +0900

    improve Leap solution

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,7 +1,15 @@
+# Better solution -------
 module Year
   def self.leap?(year)
-    return true if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
-
-    false
+    year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
   end
 end
+
+# My original solution --------
+# module Year
+#   def self.leap?(year)
+#     return true if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+#
+#     false
+#   end
+# end
