47552cac7bc07a8500e5d69514ef0d5d3909b918
Andrew Huntington <andrew.huntington@gmail.com>
2020-04-04 21:46:44 +0900

    final grains.rb

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,17 +1,39 @@
+# Cleaned up solution ---
 class Grains
-  def self.square(n)
-    raise ArgumentError, "#{n} is out-of-bounds." if n <= 0 || n > 64
+  NUMBER_OF_SQUARES = 64
 
-    2**(n-1)
-  end
+  class << self
+    def square(n)
+      raise ArgumentError, "#{n} is out-of-bounds." if n <= 0 ||
+        n > NUMBER_OF_SQUARES
 
-  def self.total
-    total_grains = 0
-
-    1.upto(64) do |n|
-      total_grains += Grains.square(n)
+      2**(n-1)
     end
 
-    total_grains
+    def total
+      (2**NUMBER_OF_SQUARES) - 1
+    end
   end
 end
+
+# Original solution ---
+# class Grains
+#   NUMBER_OF_SQUARES = 64
+#   def self.square(n)
+#     raise ArgumentError, "#{n} is out-of-bounds." if n <= 0 ||
+#       n > NUMBER_OF_SQUARES
+#
+#     2**(n-1)
+#   end
+#
+#   def self.total
+#     (2**NUMBER_OF_SQUARES) - 1
+#     total_grains = 0
+#
+#     1.upto(64) do |n|
+#       total_grains += Grains.square(n)
+#     end
+#
+#     total_grains
+#   end
+# end
