2c1d6fb93fe84de02cef4554284a54d8d90b00a4
Andrew Huntington <andrew.huntington@gmail.com>
2020-05-14 10:00:55 +0900

    finish side exercise phone_number

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,4 +1,8 @@
 module PhoneNumber
   def self.clean(number)
+    cleaned_number = number.gsub(/\D/, "").delete_prefix("1")
+    is_valid_range = !cleaned_number[0].match?(/0|1/) && !cleaned_number[3].match?(/0|1/)
+
+    cleaned_number.length == 10 && is_valid_range ? cleaned_number : nil
   end
 end
