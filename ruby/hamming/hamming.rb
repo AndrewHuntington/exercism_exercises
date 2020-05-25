8ff586cf9763c7cdac70079da49fa50f5db00ccf
Andrew Huntington <andrew.huntington@gmail.com>
2020-05-25 20:00:37 +0900

    first iteratioin of hamming.rb

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,3 +1,16 @@
 module Hamming
+  def self.compute(a, b)
+    return 0 if a == b
+    raise ArgumentError, "Strands must be of equal length." if a.size != b.size
     
+    hamming_distance = 0
+    index = 0 
+
+    while index < b.size
+      hamming_distance += 1 if a[index] != b[index]
+      index += 1
+    end
+  
+    hamming_distance
+  end
 end
