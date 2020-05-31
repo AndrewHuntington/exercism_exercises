a66b8ee294c8a7c4eae741208c3d14015ec2e552
Andrew Huntington <andrew.huntington@gmail.com>
2020-05-31 19:19:09 +0900

    first iteration for bash/error_handling.sh

--- bash/error-handling/error_handling.sh
+++ bash/error-handling/error_handling.sh
@@ -1,24 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
+number_of_arguments=$#
+name=$1
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+function main () {
+  if [[ number_of_arguments -eq 1 ]]; then
+    name=$name
+  else 
+    echo "Usage: error_handling.sh <person>"
+    exit 1
+  fi
+
+  echo "Hello, ${name}"
+}
+
+main
