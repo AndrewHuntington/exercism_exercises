9925a63b5f16c2f3a8124b5c473afa9db6aa4310
Andrew Huntington <andrew.huntington@gmail.com>
2020-07-05 23:51:36 +0900

    create ruby/scrabble-score

---
+++ ruby/scrabble-score/scrabble_score.rb
@@ -0,0 +1,15 @@
+class Scrabble
+  SCORES = {
+    %w[A E I O U L N R S T] => 1,
+    %w[D G] => 2,
+    %w[B C M P] => 3,
+    %w[F H V W Y] => 4,
+    %w[K] => 5,
+    %w[J X] => 8,
+    %w[Q Z] => 10
+  }
+
+  def initialize
+    
+  end
+end
