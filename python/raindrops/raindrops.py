eb5702620a20f414419e5ebf054c8b349d1e20f6
Andrew Huntington <andrew.huntington@gmail.com>
2020-07-06 21:15:14 +0900

    minor modification to raindrops.py

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -4,4 +4,4 @@ DROPS = {3: 'Pling', 5: 'Plang', 7: 'Plong'}
 def convert(number):
     raindrop = ''.join(DROPS[key] for key in DROPS if number % key == 0)
 
-    return str(number) if raindrop == '' else raindrop
+    return raindrop if raindrop else str(number)
