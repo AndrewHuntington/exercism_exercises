cac589e95aa6351248da1b44d03f455ed5c449b1
Andrew Huntington <andrew.huntington@gmail.com>
2020-09-05 14:24:36 +0900

    second iteration ruby/luhn

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -2,29 +2,37 @@
 
 # Takes a string and returns a boolean value based on if it is valid
 # per the Luhn formula
-module Luhn
-  module_function
-
-  def valid?(value)
-    return false if value.delete(' ').match?(/\D/) ||
-                    value.delete(' ').length <= 1
-
-    clean_value = value.delete(' ').reverse.scan(/\d/)
-
-    test_value = run_calculations(clean_value)
-
-    test_value.zero? ? true : false
+class Luhn
+  def self.valid?(value)
+    Luhn.new(value).valid?
   end
 
-  def run_calculations(values)
-    values.each.with_index do |digit, index|
+  def initialize(value)
+    @value = value.delete(' ')
+  end
+
+  def valid?
+    return false if @value.match?(/\D/) || @value.length <= 1
+
+    clean_value
+    luhn?
+  end
+
+  def clean_value
+    @cleaned_value = @value.reverse.scan(/\d/)
+  end
+
+  def luhn?
+    @luhn = @cleaned_value.each.with_index do |digit, index|
       next if index.even?
 
       if digit.to_i * 2 < 10
-        values[index] = (digit.to_i * 2).to_s
+        @cleaned_value[index] = (digit.to_i * 2).to_s
       else
-        values[index] = (digit.to_i * 2 - 9).to_s
+        @cleaned_value[index] = (digit.to_i * 2 - 9).to_s
       end
     end.reverse.map(&:to_i).reduce(:+) % 10
+
+    @luhn.zero?
   end
 end
