2e06168adb13170a580539cd658f300af9be9c0c
Andrew Huntington <andrew.huntington@gmail.com>
2021-03-03 23:19:24 +0900

    Start rna-transcription.js

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -3,6 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRna = () => {
-  throw new Error("Remove this statement and implement this function");
+export const toRna = (input) => {
+  const dna = input;
+
+  return dna;
 };
