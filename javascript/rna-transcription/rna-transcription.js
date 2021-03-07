29aede4a2a7f76e341ceaae71c72675eda479a1a
Andrew Huntington <andrew.huntington@gmail.com>
2021-03-07 15:03:43 +0900

    solve first iteration of rna-transcription.js

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,10 +1,21 @@
-//
-// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const toRna = (input) => {
-  const dna = input;
-
-  return dna;
+// Returns the RNA complement of a DNA strand.
+export const toRna = (dna) => {
+  return Array.from(dna).map(nucleotide => {
+    switch (nucleotide) {
+      case 'G':
+        return 'C';
+        break;
+      case 'C':
+        return 'G';
+        break;
+      case 'T':
+        return 'A';
+        break;
+      case 'A':
+        return 'U';
+        break;
+      default:
+        return '';
+    }
+  }).join('');
 };
