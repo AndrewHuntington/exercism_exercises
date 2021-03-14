b25affb10f52cb96bc1d091a20eb247e88f8e2b9
Andrew Huntington <andrew.huntington@gmail.com>
2021-03-14 12:03:11 +0900

    Iteration 3 for clock.rb

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -7,30 +7,16 @@ class Clock
   attr_reader :hour, :minute
 
   def initialize(hour: 0, minute: 0)
-    @hour = hour % 24
-
-    minute = handle_minute_rollover(minute) if minute > 59 || minute.negative?
-    @minute = minute
-  end
-
-  def handle_minute_rollover(minute)
-    rollover = minute / 60
-    @hour = (@hour + rollover) % 24
-    minute % 60
+    @hour = (hour + minute / 60) % 24
+    @minute = minute % 60
   end
 
   def +(other)
-    hour = @hour + other.hour
-    minute = @minute + other.minute
-
-    Clock.new(hour: hour, minute: minute)
+    Clock.new(hour: hour + other.hour, minute: minute + other.minute)
   end
 
   def -(other)
-    hour = @hour - other.hour
-    minute = @minute - other.minute
-
-    Clock.new(hour: hour, minute: minute)
+    Clock.new(hour: hour - other.hour, minute: minute - other.minute)
   end
 
   def ==(other)
