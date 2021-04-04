c909651d4942552432892a4e2451b833424e9185
Andrew Huntington <andrew.huntington@gmail.com>
2021-04-04 18:28:40 +0900

    Get twelve_days.rb to passing. Need to refactor

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -1,8 +1,36 @@
-=begin
-Write your code for the 'Twelve Days' exercise in this file. Make the tests in
-`twelve_days_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/twelve-days` directory.
-=end
+module TwelveDays
+  GIFTS = [
+    'twelve Drummers Drumming', 'eleven Pipers Piping',
+    'ten Lords-a-Leaping', 'nine Ladies Dancing',
+    'eight Maids-a-Milking', 'seven Swans-a-Swimming',
+    'six Geese-a-Laying', 'five Gold Rings', 'four Calling Birds',
+    'three French Hens', 'two Turtle Doves', 'and a Partridge in a Pear Tree'
+  ].freeze
 
+  DAYS = {
+    first: GIFTS[-1], second: GIFTS[-2..], third: GIFTS[-3..],
+    fourth: GIFTS[-4..], fifth: GIFTS[-5..], sixth: GIFTS[-6..],
+    seventh: GIFTS[-7..], eighth: GIFTS[-8..], ninth: GIFTS[-9..],
+    tenth: GIFTS[-10..], eleventh: GIFTS[-11..], twelfth: GIFTS[-12..]
+  }.freeze
+
+  def self.opening(ordinal)
+    "On the #{ordinal} day of Christmas my true love gave to me:"
+  end
+
+  def self.song
+    lyrics = []
+
+    DAYS.each do |k, v|
+      if k == :first
+        lyrics << "#{opening(k)} #{v[4..]}."
+      else
+        lyrics << "#{opening(k)} #{v.join(', ')}."
+      end
+    end
+
+    lyrics.join("\n\n") + "\n"
+  end
+end
